4d21753d066550f90a06ec7bfd73841badd89dcc
Capriskrald <hansenmann2497@hotmail.com>
2020-02-19 22:32:35 +0100

    Finished delopgave3

---
+++ AgentFinder3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgentFinder3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private Agents agentsList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //agentsList = new Agents();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (AgentListBox.SelectedIndex > 0)
+                AgentListBox.SelectedIndex--;
+        }
+
+        private void BtnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (AgentListBox.SelectedIndex < AgentListBox.Items.Count)
+                AgentListBox.SelectedIndex++;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //agentsList.AddAgent();
+            Agents agents = (Agents) this.DataContext;
+            agents.Add(new Agent());
+            AgentListBox.SelectedIndex = AgentListBox.Items.Count - 1;
+            IdBox.Focus();
+        }
+    }
+}
